2526e51d6663af716d4512a3849ea242ddf21fdd
FSDonnelly <donnelly_frank@yahoo.com>
2019-01-27 13:43:19 -0800

    installed dotenv and secured API key

--- routes/api/searchRoute.js
+++ routes/api/searchRoute.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
+require("../../.env").config();
 const axios = require("axios");
 const router = require("express").Router();
-
+const API_KEY = process.env.KEY;
 router.get("/search", (req, res) => {
   axios
-    .get(`https://www.googleapis.com/books/v1/volumes?q=Harry+Potter&orderBy=newest&printType=all&key=AIzaSyAIHQbTotLEMOBH-PjVJO8vYPHRTp2YlD4`)
+    .get(`https://www.googleapis.com/books/v1/volumes?q=Harry+Potter&orderBy=newest&printType=all&key=`${API_Key})
     .then(({ data: { results } }) => res.json(results))
     .catch(err => res.status(422).json(err));
 });
 
 module.exports = router;
-// GET https://www.googleapis.com/books/v1/volumes?q=Harry+Potter&orderBy=newest&printType=all&key=AIzaSyAIHQbTotLEMOBH-PjVJO8vYPHRTp2YlD4
